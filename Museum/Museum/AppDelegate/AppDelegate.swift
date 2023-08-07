4d477d3ae0c961bf0527d08e485e9d77bd771c28
Onur Torna <onurtorna@gmail.com>
2023-08-07 23:03:39 +0200

    Create and configure the project

---
+++ Museum/Museum/AppDelegate/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Museum
+//
+//  Created by Onur Torna on 07.08.2023.
+//
+
+import UIKit
+
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
---
+++ Museum/Museum/Screens/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Museum
+//
+//  Created by Onur Torna on 07.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
