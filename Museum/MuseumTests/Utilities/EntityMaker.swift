21b74e471cf1a89a961537e6ee61449be667b0ed
Onur Torna <onurtorna@gmail.com>
2023-08-13 13:52:12 +0200

    Add EntityMaker utility for tests

---
+++ Museum/MuseumTests/Utilities/EntityMaker.swift
@@ -0,0 +1,35 @@
+//
+//  EntityMaker.swift
+//  MuseumTests
+//
+//  Created by Onur Torna on 13.08.2023.
+//
+
+import Foundation
+@testable import Museum
+
+enum EntityMaker {
+    static func makeArtObject(
+        id: String = "",
+        title: String = "",
+        longTitle: String = "",
+        principalOrFirstMaker: String = "",
+        headerImage: ImageEntity = makeImageEntity(),
+        webImage: ImageEntity = makeImageEntity()
+    ) -> ArtObject {
+        ArtObject(
+            id: id,
+            title: title,
+            longTitle: longTitle,
+            principalOrFirstMaker: principalOrFirstMaker,
+            headerImage: headerImage,
+            webImage: webImage
+        )
+    }
+
+    static func makeImageEntity(
+        url: URL = .init(string: "www.rijksmuseum.nl")!
+    ) -> ImageEntity {
+        ImageEntity(url: url, width: 100, height: 100)
+    }
+}
